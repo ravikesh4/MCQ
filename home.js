31142394f44e45542ae11e1dd567432749b41b68
Ravikesh yadav <ravikeshyadav4@gmail.com>
2018-07-26 13:13:00 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,247 @@
+/*function show1(){
+  document.getElementById('div1').style.display ='none';
+}*/
+
+function show1(){
+  document.getElementById('divv1').style.display = 'inline';
+}
+
+function showb1(){
+    document.getElementById('divv1').style.display = 'inline';
+  document.getElementById('dib1').style.display = 'inline';
+  document.getElementById('div1').style.display = 'inline';
+
+}
+
+function show2(){
+  document.getElementById('div2').style.display = 'inline';
+  document.getElementById('divv2').style.display = 'inline';
+}
+function showb2(){
+    document.getElementById('divv2').style.display = 'inline';
+  document.getElementById('dib2').style.display = 'inline';
+  document.getElementById('dic2').style.display = 'inline';
+  document.getElementById('did2').style.display = 'inline';
+  document.getElementById('div2').style.display = 'inline';
+}
+
+
+
+function show3(){
+  document.getElementById('div3').style.display = 'inline';
+  document.getElementById('divv3').style.display = 'inline';
+}
+function showc3(){
+    document.getElementById('divv3').style.display = 'inline';
+  document.getElementById('dib3').style.display = 'inline';
+  document.getElementById('dic3').style.display = 'inline';
+  document.getElementById('did3').style.display = 'inline';
+  document.getElementById('div3').style.display = 'inline';
+}
+
+function show4(){
+  document.getElementById('div4').style.display = 'inline';
+  document.getElementById('divv4').style.display = 'inline';
+}
+function showd4(){
+    document.getElementById('divv4').style.display = 'inline';
+  document.getElementById('dib4').style.display = 'inline';
+  document.getElementById('dic4').style.display = 'inline';
+  document.getElementById('did4').style.display = 'inline';
+  document.getElementById('div4').style.display = 'inline';
+}
+
+function show5(){
+  document.getElementById('div5').style.display = 'inline';
+  document.getElementById('divv5').style.display = 'inline';
+}
+function showd5(){
+    document.getElementById('divv5').style.display = 'inline';
+  document.getElementById('dib5').style.display = 'inline';
+  document.getElementById('dic5').style.display = 'inline';
+  document.getElementById('did5').style.display = 'inline';
+  document.getElementById('div5').style.display = 'inline';
+}
+
+function show6(){
+  document.getElementById('div6').style.display = 'inline';
+  document.getElementById('divv6').style.display = 'inline';
+}
+function showd6(){
+    document.getElementById('divv6').style.display = 'inline';
+  document.getElementById('dib6').style.display = 'inline';
+  document.getElementById('dic6').style.display = 'inline';
+  document.getElementById('did6').style.display = 'inline';
+  document.getElementById('div6').style.display = 'inline';
+}
+
+function show7(){
+  document.getElementById('div7').style.display = 'inline';
+  document.getElementById('divv7').style.display = 'inline';
+}
+function showd7(){
+    document.getElementById('divv7').style.display = 'inline';
+  document.getElementById('dib7').style.display = 'inline';
+  document.getElementById('dic7').style.display = 'inline';
+  document.getElementById('did7').style.display = 'inline';
+  document.getElementById('div7').style.display = 'inline';
+}
+
+function show8(){
+  document.getElementById('div8').style.display = 'inline';
+  document.getElementById('divv8').style.display = 'inline';
+}
+function showd8(){
+    document.getElementById('divv8').style.display = 'inline';
+  document.getElementById('dib8').style.display = 'inline';
+  document.getElementById('dic8').style.display = 'inline';
+  document.getElementById('did8').style.display = 'inline';
+  document.getElementById('div8').style.display = 'inline';
+}
+
+function show9(){
+  document.getElementById('div9').style.display = 'inline';
+  document.getElementById('divv9').style.display = 'inline';
+}
+function showd9(){
+    document.getElementById('divv9').style.display = 'inline';
+  document.getElementById('dib9').style.display = 'inline';
+  document.getElementById('dic9').style.display = 'inline';
+  document.getElementById('did9').style.display = 'inline';
+  document.getElementById('div9').style.display = 'inline';
+}
+
+function show10(){
+  document.getElementById('div10').style.display = 'inline';
+  document.getElementById('divv10').style.display = 'inline';
+}
+function showd10(){
+    document.getElementById('divv10').style.display = 'inline';
+  document.getElementById('dib10').style.display = 'inline';
+  document.getElementById('dic10').style.display = 'inline';
+  document.getElementById('did10').style.display = 'inline';
+  document.getElementById('div10').style.display = 'inline';
+}
+function show11(){
+  document.getElementById('div11').style.display = 'inline';
+  document.getElementById('divv11').style.display = 'inline';
+}
+function showd11(){
+    document.getElementById('divv11').style.display = 'inline';
+  document.getElementById('dib11').style.display = 'inline';
+  document.getElementById('dic11').style.display = 'inline';
+  document.getElementById('did11').style.display = 'inline';
+  document.getElementById('div11').style.display = 'inline';
+}
+
+
+function show12(){
+  document.getElementById('div12').style.display = 'inline';
+  document.getElementById('divv12').style.display = 'inline';
+}
+function showd12(){
+    document.getElementById('divv12').style.display = 'inline';
+  document.getElementById('dib12').style.display = 'inline';
+  document.getElementById('dic12').style.display = 'inline';
+  document.getElementById('did12').style.display = 'inline';
+  document.getElementById('div12').style.display = 'inline';
+}
+
+function show13(){
+  document.getElementById('div13').style.display = 'inline';
+  document.getElementById('divv13').style.display = 'inline';
+}
+function showd13(){
+    document.getElementById('divv13').style.display = 'inline';
+  document.getElementById('dib13').style.display = 'inline';
+  document.getElementById('dic13').style.display = 'inline';
+  document.getElementById('did13').style.display = 'inline';
+  document.getElementById('div13').style.display = 'inline';
+}
+
+
+function show14(){
+  document.getElementById('div14').style.display = 'inline';
+  document.getElementById('divv14').style.display = 'inline';
+}
+function showd14(){
+    document.getElementById('divv14').style.display = 'inline';
+  document.getElementById('dib14').style.display = 'inline';
+  document.getElementById('dic14').style.display = 'inline';
+  document.getElementById('did14').style.display = 'inline';
+  document.getElementById('div14').style.display = 'inline';
+}
+
+function show15(){
+  document.getElementById('div15').style.display = 'inline';
+  document.getElementById('divv15').style.display = 'inline';
+}
+function showd15(){
+    document.getElementById('divv15').style.display = 'inline';
+  document.getElementById('dib15').style.display = 'inline';
+  document.getElementById('dic15').style.display = 'inline';
+  document.getElementById('did15').style.display = 'inline';
+  document.getElementById('div15').style.display = 'inline';
+}
+
+
+
+function show16(){
+  document.getElementById('div16').style.display = 'inline';
+  document.getElementById('divv16').style.display = 'inline';
+}
+function showd16(){
+    document.getElementById('divv16').style.display = 'inline';
+  document.getElementById('dib16').style.display = 'inline';
+  document.getElementById('dic16').style.display = 'inline';
+  document.getElementById('did16').style.display = 'inline';
+  document.getElementById('div16').style.display = 'inline';
+}
+
+function show17(){
+  document.getElementById('div17').style.display = 'inline';
+  document.getElementById('divv17').style.display = 'inline';
+}
+function showd17(){
+    document.getElementById('divv17').style.display = 'inline';
+  document.getElementById('dib17').style.display = 'inline';
+  document.getElementById('dic17').style.display = 'inline';
+  document.getElementById('did17').style.display = 'inline';
+  document.getElementById('div17').style.display = 'inline';
+}
+
+function show18(){
+  document.getElementById('div18').style.display = 'inline';
+  document.getElementById('divv18').style.display = 'inline';
+}
+function showd18(){
+    document.getElementById('divv18').style.display = 'inline';
+  document.getElementById('dib18').style.display = 'inline';
+  document.getElementById('dic18').style.display = 'inline';
+  document.getElementById('did18').style.display = 'inline';
+  document.getElementById('div18').style.display = 'inline';
+}
+
+function show19(){
+  document.getElementById('div19').style.display = 'inline';
+  document.getElementById('divv19').style.display = 'inline';
+}
+function showd19(){
+    document.getElementById('divv19').style.display = 'inline';
+  document.getElementById('dib19').style.display = 'inline';
+  document.getElementById('dic19').style.display = 'inline';
+  document.getElementById('did19').style.display = 'inline';
+  document.getElementById('div19').style.display = 'inline';
+}
+
+function show20(){
+  document.getElementById('div20').style.display = 'inline';
+  document.getElementById('divv20').style.display = 'inline';
+}
+function showd20(){
+    document.getElementById('divv20').style.display = 'inline';
+  document.getElementById('dib20').style.display = 'inline';
+  document.getElementById('dic20').style.display = 'inline';
+  document.getElementById('did20').style.display = 'inline';
+  document.getElementById('div20').style.display = 'inline';
+}
